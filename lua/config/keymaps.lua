fec75a75a3c70a5e66b8f506fa0a18e348adf567
Thrilok emmadisetty <63156546+Thrilok28021996@users.noreply.github.com>
2024-10-29 19:49:13 +0530

    My Lazyvim configv1.0

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+--
+local map = vim.keymap.set
+-- For conciseness
+local opts = { noremap = true, silent = true }
+-- quit file
+vim.keymap.set("n", "<C-q>", "<cmd> q <CR>", opts)
+
+-- Navigate buffers
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
+-- Comment
+map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
+map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
